6684d9999420f60707589ff6ab1a4d89dbb2ac6e
Benny <blaze349@protonmail.com>
2017-04-24 13:49:12 +0000

    Factorial algo, could use improvement

---
+++ math/factorialOf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int factorialOf(int num) {
+    //if num is equal to one
+    if (num < 1) {
+        //wil change later to include the factorial of 0
+        return 0;
+    }
+    if (num == 1) {
+        return 1;
+    } else {
+        return num * factorialOf(num-1);
+    }
+}
+
+int main(int argc, char**argv) {
+    printf("%d", factorialOf(atoi(argv[1])));
+}
