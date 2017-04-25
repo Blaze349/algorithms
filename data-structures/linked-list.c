eec11e29e5fb10c15bb4f8d9128521a66bc294e2
Benny <blaze349@protonmail.com>
2017-04-25 09:47:05 +0000

    ...

---
+++ data-structures/linked-list.c
@@ -0,0 +1,25 @@
+struct node {
+    int val;
+    struct node* prev;
+    struct node* next;
+}
+
+void insertAtLL() {
+    
+}
+
+void deleteLL() {
+    
+}
+
+void searchLL() {
+    
+}
+
+void displayLL() {
+    
+}
+
+void deleteAtLL() {
+    
+}
