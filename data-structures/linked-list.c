e4cf0e99e809cd6abdff3697ce0323762b996ad6
Benny <blaze349@protonmail.com>
2017-04-25 11:15:12 +0000

    ...

--- data-structures/linked-list.c
+++ data-structures/linked-list.c
@@ -1,7 +1,24 @@
-struct node {
-    int val;
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node {
+    int data;
     struct node* prev;
     struct node* next;
+}
+
+node* createNode(int data, node* next) {
+    node* newNode = malloc(sizeof(node*));
+    
+    if (newNode == NULL) {
+        exit(1);
+    }
+    
+    newNode->data = data;
+    newNode->next = next;
+    
+    return newNode;
 }
 
 void insertAtLL() {
@@ -23,3 +40,7 @@ void displayLL() {
 void deleteAtLL() {
     
 }
+
+int main(void {
+    node* head = createNode(NULL, NULL);
+}                               
